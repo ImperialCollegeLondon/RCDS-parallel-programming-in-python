c1f29c87c1391bc91201cd6fcd368530e15a85fd
Chris Cooling <cmcooling@gmail.com>
2025-02-18 14:18:37 +0000

    Updated sine approximation to use starmap

--- sample_solutions/sine_approximation.py
+++ sample_solutions/sine_approximation.py
@@ -1,27 +1,25 @@
 from multiprocessing import Process, Pool
 from math import factorial, pi, sin
 
-def sine_term(inputs):
+def sine_term(x, k):
     # This function calculates the nth term of the Taylor series expansion of sine(x)
-    # The inputs are a list (x, n) where x is the value at which to evaluate the sine function
+    # x is the value at which to evaluate the sine function
+    # k is the index of the term to calculate
 
-    # Extract x and n from the inputs list
-    x = inputs[0]
-    n = inputs[1]
     # Calculate and return the term
-    return ((-1) ** n) * (x ** (2 * n + 1)) / factorial(2 * n + 1)
+    return ((-1) ** k) * (x ** (2 * k + 1)) / factorial(2 * k + 1)
 
 def sine_approx(x, n_terms):
     # This function calculates an approximation of the sine function at x using n_terms terms of the Taylor series expansion
 
     # Create a list of inputs for the sine_term function
-    inputs = [[x, n] for n in range(n_terms)]
+    inputs = [(x, k) for k in range(n_terms)]
     print(inputs)
 
     # Create a pool of two threads to calculate the terms in parallel
     with Pool(2) as p:
         # Calculate the terms in parallel and store the results in a list
-        terms = p.map(sine_term, inputs)
+        terms = p.starmap(sine_term, inputs)
 
     # Return the sum of the terms
     return sum(terms)
